2b3b0792b126ccc114a3cdbbbc096fc682227cdf
Bendik Lynghaug <bendik.lynghaug@gmail.com>
2017-10-03 19:10:52 -0400

    remove safari hack fixed upstream

--- transform/index.js
+++ transform/index.js
@@ -7,14 +7,9 @@ module.exports = transform
 function transform (opts) {
   var triggered = false
   return through(function (chunk, enc, next) {
-    var string = chunk.toString()
-    if (!triggered && string.indexOf('<html') === 0) {
+    if (!triggered && chunk.toString().indexOf('<html') === 0) {
       triggered = true
       var msg = '<html lang="no" dir="ltr"><script src="https://api.mapbox.com/mapbox-gl-js/v0.40.1/mapbox-gl.js"></script><link href="https://api.mapbox.com/mapbox-gl-js/v0.40.1/mapbox-gl.css" rel="stylesheet" />'
-      this.push(new Buffer(msg))
-      next()
-    } else if (string.indexOf(`onload="this.rel='stylesheet'"`) > 0) {
-      var msg = string.replace(`<link rel="preload" as="style"`, '<link rel="stylesheet"')
       this.push(new Buffer(msg))
       next()
     } else {
