ae45f6d9f52ffd46bdf9f4a94c79fa3853c16edd
Bendik Lynghaug <bendik.lynghaug@gmail.com>
2018-04-22 13:21:41 -0400

    update mapbox dependency

--- headertags.js
+++ headertags.js
@@ -6,8 +6,8 @@ function headertags (opts) {
   return hyperstream({
     'head': {
       _appendHtml: `
-        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.css' rel='stylesheet' />
-        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.1/mapbox-gl.js' defer onload="window.mapboxgl.accessToken='${process.env.MAPBOX_TOKEN}'"></script>
+        <link href='//api.tiles.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.css' rel='stylesheet' />
+        <script src='//api.tiles.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.js' defer onload="window.mapboxgl.accessToken='${process.env.MAPBOX_TOKEN}'"></script>
       `
     }
   })
