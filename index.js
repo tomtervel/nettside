956bbda99852318bb66c0756b36e40a8a21ee4eb
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2018-05-19 12:51:14 -0500

    refactor some view logic

--- index.js
+++ index.js
@@ -22,21 +22,23 @@ css('./app.css')
 
 var content = hypha.readSiteSync('./content', { parent: 'content' })
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(require('choo-devtools')({
+    filter: function (event) {
+      return event !== 'DOMTitleChange'
+    }
+  }))
+} else {
+  // app.use(require('choo-service-worker')())
+}
 app.use(wrapper(content))
 app.use(init)
 
-Object.keys(content).forEach(function (path) {
-  if (path === '/partials') return
+for (var path in content) {
+  if (path === '/partials') continue
   app.route(path, mainView)
-})
-
-app.route('/*', mainView)
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
 }
+app.route('*', mainView)
 
 module.exports = app.mount('body')
 
@@ -47,7 +49,7 @@ function init (state, emitter) {
     tomterMap = new MapBox()
     emitter.emit(state.events.RENDER)
     emitter.on(state.events.PUSHSTATE, function () {
-      window.scrollTo(0,0)
+      window.scrollTo(0, 0)
     })
   })
 }
@@ -62,63 +64,62 @@ function wrapper (siteContent) {
 }
 
 function mainView (state, emit) {
-  var page = state.page().value()
-  emit(state.events.DOMTITLECHANGE, `${state.site}${page.tittel ? ` | ${page.tittel}` : ''}`)
+  var tittel = state.content[state.href || '/'] ? state.content[state.href || '/'].tittel : '404'
+  emit(state.events.DOMTITLECHANGE, `${state.site}${tittel ? ` | ${tittel}` : ''}`)
   return html`
     <body class="vh-100 flex flex-column justify-between items-center bg-washed-yellow black sans-serif">
       ${header(state, emit)}
-      ${!state.content[state.href || '/']
-        ? fourOhFour()
-        : [
-          page.kart && page.kart.length >= 2
-            ? tomterMap
-              ? [ 
-                html`<div class="skew-y origin-top-left h4 bg-washed-yellow w-100 z-1" style="margin-top: -8rem; margin-bottom: -5rem;"></div>`, 
-                tomterMap.render(page.kart)
-              ]
-              : [
-                html`<div class="skew-y origin-top-right h4 bg-washed-yellow w-100 z-1" style="margin-top: -8rem; margin-bottom: -5rem;"></div>`,
-                html`<div class="w-100 vh-50 bg-map-accent z-0"></div>`
-              ]
-            : null,
-          pageContent(state, emit)
-        ]
-      }
+      ${contentView(state, emit)}
       ${footer(state.partials.footer || '')}
     </body>
   `
 }
 
-function pageContent (state, emit) {
-  var currentPage = state.page().value()
-  return html`
-    <main class="w-100 pb5-l pb3 f6 f5-ns f4-l relative flex flex-column items-center bg-animate z-2" id="content">
-      <article class="mw8 ph4-ns ph2 ph5-l pb3 pb5-ns w-100">
-        <header class="flex flex-column mb4 mb5-ns cf">
-          <div
-            class="skew-y origin-top-right w-100 pv3 bg-vel-blue ph2 white z-1"
-            style=${currentPage.kart ? 'margin-top: -3rem' : ''}>
-            <h1 class="skew-counter origin-top-right w-100 mv0 bg f-1 f-4-ns fw4 tc rotate-tiny origin-top-right ">${currentPage.tittel}</h1>
-          </div>
-          ${ currentPage.dato
-            ? html`
-              <div class="skew-y self-end w-50 origin-top-right bg-white vel-blue z-1 ba bw1" style="margin-top: -.75rem; margin-bottom: -1rem;">
-                <h3 class="fw4 tr f-2 mv1 mh2 skew-counter" rel="date">${currentPage.dato}</h3>
-              </div>
-            ` 
-            : null
-          }
-        </header>
-        ${raw(md.render(currentPage.beskrivelse || ''))}
-        ${state.href !== '/'
-          ? state.page().pages().sortBy('url', 'desc').toArray().map(pageListing)
-          : null
-        }
-      </article>
-    </main>
-  `
+function contentView (state, emit) {
+  var page = state.page().value()
+  if (Object.keys(page).length === 0) return fourOhFour()
+  var downloads = Object.keys(page.files).filter(function (file) {
+    return !file.match('.jpg')
+  }).map(function (file) {
+    return page.files[file]
+  })
+  return [
+    html`<div 
+      class="${page.kart || 'dn'} skew-y origin-top-left h4 bg-washed-yellow w-100 z-1" 
+      style="margin-top: -8rem; margin-bottom: -5rem;">
+    </div>`,
+    mapView(page.kart, tomterMap),
+    html`
+      <main class="w-100 pb5-l pb3 f6 f5-ns f4-l relative flex flex-column items-center bg-animate z-2" id="content">
+        <article class="mw8 ph4-ns ph2 ph5-l pb3 pb5-ns w-100">
+          <header class="flex flex-column mb4 mb5-ns cf">
+            <div
+              class="skew-y origin-top-right w-100 pv3 bg-vel-blue ph2 white z-1"
+              style=${page.kart ? 'margin-top: -3rem' : ''}>
+              <h1 class="skew-counter origin-top-right w-100 mv0 bg f-1 f-4-ns fw4 tc rotate-tiny origin-top-right ">${page.tittel}</h1>
+            </div>
+            <div class="${page.dato || 'dn'} skew-y self-end w-50 origin-top-right bg-white vel-blue z-1 ba bw1" style="margin-top: -.75rem; margin-bottom: -1rem;">
+              <h3 class="fw4 tr f-2 mv1 mh2 skew-counter" rel="date">${page.dato}</h3>
+            </div> 
+          </header>
+          ${raw(md.render(page.beskrivelse || ''))}
+          <section class=${downloads.length ? 'mv4' : 'dn'} rel="files">
+            ${downloads.map(fileDownload)}
+          </section>
+          ${page.url === '/' ? null : state.page().pages().sortBy('url', 'desc').toArray().map(pageListing)}
+        </article>
+      </main>
+    `
+  ]
 }
 
+function mapView (coords, component) {
+  if (!coords) return null
+  if (!component) return html`<div class="w-100 vh-50 bg-map-accent z-0"></div>`
+  return [
+    component.render(coords)
+  ]
+}
 
 function header (state, emit) {
   return html`
@@ -143,25 +144,22 @@ function header (state, emit) {
 }
 
 function menuElements (state, emit) {
-  return html`
-    <ul class="list ma0 flex flex-column flex-wrap-ns flex-row-ns pl0 pl4-ns lh-title content-end justify-center align-center">
-      ${Object.keys(state.content).sort().map(function (path) {
-        var pathArray = path.split('/')
-        var isCurrent = path !== state.href
-        if (path === '/' || pathArray.length > 2) return null
-        return html`
-          <li class="mv2 mt0-ns tr mv1-ns">
-            <a
-              href=${path}
-              class="${state.href.indexOf(pathArray[1]) !== 1 ? 'pointer' : 'bg-light-yellow'} link b black pa1 ml2-ns hover-bg-light-yellow bn bb-ns bg-animate"
-            >
-              ${state.content[path].tittel}
-            </a>
-          </li>
-        `
-      })}
-    </ul>
-  `
+  return html`<ul class="list ma0 flex flex-column flex-wrap-ns flex-row-ns pl0 pl4-ns lh-title content-end justify-center align-center">
+    ${Object.keys(state.content).sort().map(navElement)}
+  </ul>`
+  function navElement (path) {
+    var pathArray = path.split('/')
+    if (path === '/' || pathArray.length > 2) return null
+    var isCurrent = state.href.indexOf(pathArray[1]) === 1
+    return html`<li class="mv2 mt0-ns tr mv1-ns">
+      <a
+        href=${path}
+        class="${isCurrent ? 'bg-light-yellow' : 'pointer'} link b black pa1 ml2-ns hover-bg-light-yellow bn bb-ns bg-animate">
+        ${state.content[path].tittel}
+        </a>
+      </li>
+    `
+  }
 }
 
 function pageListing (page) {
@@ -172,17 +170,27 @@ function pageListing (page) {
       <a class="link vel-blue" href=${page.url}>
         <h1 class="mb0">${page.tittel}</h1>
       </a>
-      ${page.dato ? html`<h5>${page.dato}</h5>` : null }
+      ${page.dato ? html`<h5>${page.dato}</h5>` : null}
       ${raw(md.render(page.beskrivelse))}
     </section>
   `
+}
+
+function fileDownload (file) {
+  return html`<a
+    href=${file.path} 
+    rel="download"
+    target="_blank"
+    class="link pa2 bg-white mh-2 br2 ba b download">
+    ${file.name}
+  </a>`
 }
 
 function footer (markdown) {
   assert.equal(typeof markdown, 'string', `markdown should be type string, but was ${JSON.stringify(markdown)}`)
   return html`
     <footer class="w-100 flex flex-column items-center bg-vel-blue washed-yellow">
-      <div class="mw8 pv4 ph4-ns ph2 ph5-l lh-copy">
+      <div class="mw8 f6 f5-ns pv4 ph4-ns ph2 ph5-l lh-copy">
         ${raw(md.render(markdown))}
       </div>
     </footer>
@@ -192,7 +200,11 @@ function footer (markdown) {
 function fourOhFour () {
   return html`
     <main class="w-100 tc pv6 ph3 red bg-animate" id="content">
-      <h1 class="fw2">Vel, vel, vel…<br />Siden du søkte har enten flyttet på seg eller finnes ikke lenger.</h1>
+      <h1 class="fw2">
+        Vel, vel, vel…
+        <br/>
+        Siden du ser etter har kanskje flyttet seg til et annet sted.
+      </h1>
     </main>
   `
 }
