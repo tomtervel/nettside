07cf14543136ff459f6b9670a67751aa1e9983f9
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2020-11-11 12:09:16 +0100

    fikset julegransyntax med manglende mellomrom

--- headertags.js
+++ headertags.js
@@ -6,8 +6,8 @@ function headertags (opts) {
   return hyperstream({
     head: {
       _appendHtml: `
-        <script src='https://api.mapbox.com/mapbox-gl-js/v1.10.0/mapbox-gl.js' defer onload="window.mapboxgl.accessToken='${process.env.MAPBOX_TOKEN}'"></script>
-        <link href='https://api.mapbox.com/mapbox-gl-js/v1.10.0/mapbox-gl.css' rel='stylesheet' />
+        <script src='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js' defer onload="window.mapboxgl.accessToken='${process.env.MAPBOX_TOKEN}'"></script>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
       `
     }
   })
