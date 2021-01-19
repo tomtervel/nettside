be4d2aaa290075c58642cc5981a1a9aee3a644b5
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2021-01-19 10:06:49 +0100

    link not meta for icon head tag

--- headertags.js
+++ headertags.js
@@ -6,7 +6,7 @@ function headertags (opts) {
   return hyperstream({
     head: {
       _appendHtml: `
-        <meta rel='icon' href='/assets/favicon.svg'/>
+        <link rel='icon' href='/assets/favicon.svg'/>
         <script src='https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.js' defer onload="window.mapboxgl.accessToken='${process.env.MAPBOX_TOKEN}'"></script>
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css' rel='stylesheet' />
       `
