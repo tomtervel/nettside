6a2be241da6120edf2aeba2b705469476d41d3d8
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2021-03-07 22:45:22 +0100

    fixed testserver

--- testserver.js
+++ testserver.js
@@ -9,8 +9,10 @@ process.env.NODE_ENV = 'test'
 const compiler = bankai(path.join(__dirname, 'index.js'))
 const server = http.createServer(function (req, res) {
   if (req.url === '/test/pass') {
+    console.log('visual verification 👍')
     process.exit(0)
   } else if (req.url === '/test/fail') {
+    console.log('visual verification 👎')
     process.exit(1)
   }
   compiler(req, res, function () {
@@ -20,12 +22,13 @@ const server = http.createServer(function (req, res) {
 })
 
 server.listen(8080, function () {
+  console.log('Server started, your browser should open to "http://localhost:8080"')
   if (process.platform === 'darwin') {
     osascript.execute('tell application "Safari" to open location "http://localhost:8080/"', function (err, result) {
       if (err) throw err
     })
   } else {
-    const { error } = spawnSync('open "http://localhost:8080/"')
+    const { error } = spawnSync('firefox', ['http://localhost:8080/'])
     if (error) throw error
   }
 })
